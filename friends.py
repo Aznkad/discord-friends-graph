417eef827ddbe3ebb537de28a4c648f12b17cf3d
Thibaut Antoine <thibaut-antoine@ens-rennes.fr>
2023-06-15 10:42:35 +0100

    added the main script

---
+++ friends.py
@@ -0,0 +1,58 @@
+import discord
+from unidecode import unidecode
+
+client = discord.Client()
+
+def graph_from_dict(graph_dict):
+    filename = "graph.dot"
+    graph_items = graph_dict.items()
+
+    with open(filename, "w", encoding="utf-8") as file:
+        file.write("strict graph{\n")
+        for (k, v) in graph_items:
+            file.write(f"{k}[label=\"{k}\n{len(v)}\"]\n")
+        file.write("\n")
+
+        for (k, v) in graph_items:
+            for name in v:
+                file.write(f"{k} -- {name}\n")
+        file.write("}\n")
+
+def normalize(s):
+    special_chars = "&~#{}()[]-'\"@^$£€<>*+=|_,;:!?./%"
+    s = unidecode(s) # Remove accents, emojis...
+    for c in special_chars:
+        s.replace(c, '')
+    return s
+
+
+@client.event
+async def on_ready():
+    friends_graph = dict()
+    filename = "friends.txt"
+
+    print(f'We have logged in as {client.user}')
+
+    with open(filename, "w", encoding="utf-8") as file:
+
+        # Treating each separate friend
+        for friend in client.friends:
+            mutual_friends_list = []
+            friend_name = normalize(friend.user.name)
+
+            print(f"Treating {friend_name}")
+            file.write(f"{friend_name}: ")
+
+            profile = await client.fetch_user_profile(friend.user.id)
+            for mutual_friend in profile.mutual_friends:
+                mutual_friend_name = normalize(mutual_friend.name)
+                mutual_friends_list.append(mutual_friend_name)
+                file.write(f"{mutual_friend_name}, ")
+            friends_graph[friend_name] = mutual_friends_list
+            file.write("\n")
+
+    print("Finished!")
+    graph_from_dict(friends_graph)
+
+
+client.run('token')
